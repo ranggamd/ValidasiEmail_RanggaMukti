166e7a71c8c1fbf6c09ce031205db6ac3ed9155b
ranggamd <77499161+ranggamd@users.noreply.github.com>
2021-07-22 00:27:41 +0700

    Add files via upload

---
+++ Validasi Email.py
@@ -0,0 +1,17 @@
+import re
+
+#bisa cek selain gmail.com, contoh : @yahoo.com dll
+regex = '^[a-z0-9]+[/._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+
+#Ketika kita hanya ingin cek khusus gmail.com saja
+#regex = '^[a-z0-9]+[/._]?[a-z0-9]+[@]+[gmail]+[.]\w{2,3}$'
+
+def cek(email):
+    if (re.search(regex,email)):
+        print("Email Valid")
+    else:
+        print("Email Tidak Valid")
+
+if __name__ == "__main__":
+    email = input("Masukan Email Address: ")
+    cek(email)
